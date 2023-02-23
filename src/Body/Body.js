5228c7ef01e1a954eaed6eb79a252ec00ccf9649
ArtjomLemonezoArt <mrsintesthebest@gmail.com>
2023-02-23 20:13:23 +0200

    Homework& Highprice& chart & countdown& Loading

--- src/Body/Body.js
+++ src/Body/Body.js
@@ -1,31 +1,34 @@
 import { useState, useEffect } from 'react';
 import {
-     LineChart, 
-     CartesianGrid, 
-     XAxis, 
-     YAxis, 
-     Tooltip, 
-     Legend, 
-     Line, 
-     ResponsiveContainer, 
-     ReferenceLine,
-     ReferenceArea } from 'recharts';
+    LineChart,
+    CartesianGrid,
+    XAxis,
+    YAxis,
+    Tooltip,
+    Legend,
+    Line,
+    ResponsiveContainer,
+    ReferenceLine,
+    ReferenceArea
+} from 'recharts';
 import { getPriceData } from '../services/apiService';
 import ErrorModal from '../ErrorModal';
 import moment from 'moment';
 
-function Body({ hourRange }) {
+function Body({ hourRange, activePrice, setLowPriceTimestamp }) {
 
-    const [data, setData ] = useState([]);
-    const [errorMessage, setErrorMessage ] = useState(null);
+    const [data, setData] = useState([]);
+    const [errorMessage, setErrorMessage] = useState(null);
     const [x1, setX1] = useState(0);
+    const [xHigh, setXHigh] = useState([]);
 
     useEffect(() => {
         getPriceData()
-            .then(({success, data, messages}) => {
+            .then(({ success, data, messages }) => {
                 if (!success) {
                     throw messages[0];
                 }
+
 
                 const newData = data.ee.map(d => {
                     return {
@@ -36,31 +39,55 @@ function Body({ hourRange }) {
                     }
                 });
 
-                const timeStampNow = moment().unix();
-                const futureData = newData.filter((el) => el.timestamp > timeStampNow);
-                const rangePrices = [];
-                
-                futureData.forEach((v, i, arr) => {
-
-                    const range = arr.slice(i, i + hourRange + 1);
-                    if(range.length === hourRange + 1){
-                        let sum = 0;
-                        range.forEach(v => sum += v.price);
-                        rangePrices.push({ sum, i});
-                        
-                    }
-
-                });
-
-                rangePrices.sort((a, b) => a.sum - b.sum);
-
-                setX1(rangePrices[0].i);
-
                 setData(newData);
             })
             .catch((error) => setErrorMessage(error.toString()));
-    }, [hourRange]);
-    
+    }, []);
+
+    useEffect(() => {
+        if (data.length) {
+            const timeStampNow = moment().unix();
+
+            const futureData = data.filter((el) => el.timestamp > timeStampNow);
+
+            const hourRangeLocal = activePrice === 'low' ? hourRange : 1;
+
+            const rangePrices = [];
+
+            futureData.forEach((v, i, arr) => {
+                const range = arr.slice(i, i + hourRangeLocal);
+                if (range.length === hourRangeLocal) {
+                    let sum = 0;
+                    range.forEach(v => sum += v.price);
+                    rangePrices.push({ sum, i, timestamp: v.timestamp });
+
+                }
+
+            });
+
+            rangePrices.sort((a, b) => a.sum - b.sum);
+
+            if (activePrice === 'low') {
+                setX1(rangePrices[0].i);
+                setLowPriceTimestamp(rangePrices[0].timestamp);
+                setXHigh([]);
+            }
+            else {
+
+                rangePrices.reverse();
+                const half = rangePrices.slice(0, rangePrices.length / 2);
+                let sum = 0;
+                half.forEach(v => {
+                    sum += v.sum;
+                });
+                let average = sum / half.length;
+                setXHigh(half.filter(v => v.sum > average));
+            }
+
+        }
+
+    }, [hourRange, data, activePrice, setLowPriceTimestamp]);
+
     return (
         <>
             <ResponsiveContainer width="100%" height={400}>
@@ -72,10 +99,16 @@ function Body({ hourRange }) {
                     <Legend />
                     <Line type="monotone" dataKey="price" stroke="#8884d8" />
                     <ReferenceLine x={data.findIndex((el) => el.current)} stroke="red" />
-                    <ReferenceArea x1={x1 + 10} x2={x1 + hourRange + 10} stroke="green" fill="green" strokeOpacity={0.3} fillOpacity={0.2} />
+                    {xHigh.length ? xHigh.map(x => (
+                        <ReferenceArea key={x1} x1={x.i + 10} x2={x.i + 10 + 1} stroke="red" fill="red" strokeOpacity={0.3} fillOpacity={0.2} />
+                    )) :
+
+                        (
+                            <ReferenceArea x1={x1 + 10} x2={x1 + hourRange + 10} stroke="green" fill="green" strokeOpacity={0.3} fillOpacity={0.2} />
+                        )}
                 </LineChart>
             </ResponsiveContainer>
-            <ErrorModal errorMessage={errorMessage} handleClose={() => setErrorMessage(null)}/>
+            <ErrorModal errorMessage={errorMessage} handleClose={() => setErrorMessage(null)} />
         </>
     );
 }
